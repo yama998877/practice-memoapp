8a4c065537b88f0b17a19bfa8d43852d7bbf094f
yama998877 <yamada.sss.369@gmail.com>
2023-11-30 10:51:08 +0900

    app.rbの修正
    不要なコード、コメントの削除
    URLの改善
    HTMLエスケープ処理機能の修正
    ./data以下のファイル以外のファイルを消せないようにif文を追加

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require 'sinatra/reloader'
 require 'json'
 require 'securerandom'
 require 'rack/utils'
+require 'pathname'
 set :enviroment, :production
 
 helpers do
@@ -17,7 +18,6 @@ def save_memo(filename, word)
   File.open(filename, 'w') do |file|
     file.puts(word)
   end
-  # File.open(filename).close
 end
 
 def read_memo
@@ -29,11 +29,11 @@ def read_memo
   end
 end
 
-get '/' do
+get '/memos' do
   json_files = []
   @memo_path = Dir.glob('./data/*.json').sort_by { |f| File.mtime(f) }
   @memo_path.each do |file|
-    p json_files << File.read(file)
+    json_files << File.read(file)
   end
 
   hashs = []
@@ -49,55 +49,49 @@ get '/' do
   erb :index
 end
 
-post '/' do
+post '/memos' do
   memo_hash = {}
   @title = params['title']
-  @title << 'タイトルがありません' if @title.strip.empty?
-  memo_hash['title'] = h(params['title'])
-  memo_hash['detail'] = h(params['detail'])
+  memo_hash['title'] = params['title']
+  memo_hash['detail'] = params['detail']
   save_memo("./data/#{SecureRandom.uuid}.json", memo_hash.to_json)
   @names = read_memo
   @memo_path = Dir.children('./data/').sort_by { |f| File.mtime("./data/#{f}") }
-  redirect '/'
-  erb :index
+  redirect '/memos'
 end
 
-patch '/' do
+patch '/memos' do
   new_memo_hash = {}
-  params['title'] << 'タイトルがありません' if params['title'].strip.empty?
-  new_memo_hash['title'] = h(params['title'])
-  new_memo_hash['detail'] = h(params['detail'])
+  new_memo_hash['title'] = params['title']
+  new_memo_hash['detail'] = params['detail']
   update_file = params['json']
   changing_content = new_memo_hash.to_json
   save_memo("./data/#{update_file}", changing_content)
 
-  redirect '/'
+  redirect '/memos'
 end
 
-delete '/' do
-  params
+delete '/memos' do
   delete_file = params['json']
-  File.delete("./data/#{delete_file}")
-  redirect '/'
+  delete_path = Pathname.new("./data/#{delete_file}")
+  File.delete(delete_path) if delete_path.dirname.to_s == './data'
+  redirect '/memos'
 end
 
-get '/new' do
+get '/memos/new' do
   erb :new
 end
 
-get '/data/:file' do
+get '/memos/data/:file' do
   @json_file = params[:file]
 
   @memo_detail = JSON.parse(File.read("./data/#{@json_file}"))
-  @memo_detail['title']
   erb :detail
 end
 
-get '/new/data/:file' do
+get '/memos/new/data/:file' do
   @file_name = params[:file]
   @memo = JSON.parse(File.read("./data/#{@file_name}"))
-  @memo['title']
-  @memo['detail']
   erb :new_detail
 end
 
