cc5f63898f13c9c70013fbe7bc09d1a37729ab39
yama998877 <yamada.sss.369@gmail.com>
2024-05-17 08:58:15 +0900

    app.rbの修正
    ハッシュのkeyを文字列からsymbolになるように変更。
    read_memoメソッドに:uuidの値がuuidの時、条件が一致するか確認するコードを追加。
    ルーティングのパスの`:file`を`:id`に変更。
    メモの変更時に新しいメモのファイルが追加できないように条件を追加。
    削除の時の条件にread_memoをしようするように変更。
    @memoをread_memoをしようするように変更。

--- app.rb
+++ app.rb
@@ -18,14 +18,14 @@ DATA_DIR = File.join(File.dirname(__FILE__), 'data')
 
 def read_memos
   Dir.glob("#{DATA_DIR}/*.json").sort_by { |f| File.mtime(f) }.map do |f|
-    memo = JSON.parse(File.read(f))
+    memo = JSON.parse(File.read(f), symbolize_names: true)
     memo[:uuid] = f.gsub(%r{./data/|.json}, '')
     memo
   end
 end
 
 def read_memo(memo_uuid)
-  read_memos.detect { |memo_data| memo_data.value?(memo_uuid) }
+  read_memos.detect { |memo_data| memo_data.value?(memo_uuid) if memo_data[:uuid] == memo_uuid }
 end
 
 def write_memo(filename, word)
@@ -37,14 +37,14 @@ def write_memo(filename, word)
 end
 
 get '/memos' do
-  @url_titles = read_memos.map { |memo_url| memo_url.values_at('title', :uuid) }
+  @url_titles = read_memos.map { |memo_url| memo_url.values_at(:title, :uuid) }
   erb :index
 end
 
 post '/memos' do
   new_memo = {
-    'title' => params['title'],
-    'detail' => params['detail']
+    title: params[:title],
+    detail: params[:detail]
   }
   write_memo("./data/#{SecureRandom.uuid}.json", new_memo.to_json)
   redirect '/memos'
@@ -54,34 +54,33 @@ get '/memos/new' do
   erb :new
 end
 
-get '/memos/:file' do
-  @json_file = params[:file]
+get '/memos/:id' do
+  @json_file = params[:id]
   @memo_detail = read_memo(@json_file)
   erb :detail
 end
 
-patch '/memos/:file' do
+patch '/memos/:id' do
+  uuid = params[:id]
   edited_memo = {
-    'title' => params['title'],
-    'detail' => params['detail']
+    title: params[:title],
+    detail: params[:detail]
   }
-  edited_file_name = "./data/#{params[:file]}.json"
+  edited_file_name = "./data/#{uuid}.json"
   changing_content = edited_memo.to_json
-  write_memo(edited_file_name, changing_content)
-
+  write_memo(edited_file_name, changing_content) if !read_memo(uuid).nil?
   redirect '/memos'
 end
 
-delete '/memos/:file' do
-  uuid = params[:file]
-  uuids = read_memos.map { |memo| memo[:uuid] }
-  File.delete("./data/#{uuid}.json") if uuids.include?(uuid)
+delete '/memos/:id' do
+  uuid = params[:id]
+  File.delete("./data/#{uuid}.json") if !read_memo(uuid).nil?
   redirect '/memos'
 end
 
-get '/memos/:file/edit' do
-  @file_name = params[:file]
-  @memo = JSON.parse(File.read("./data/#{@file_name}.json"))
+get '/memos/:id/edit' do
+  @file_name = params[:id]
+  @memo = read_memo(@file_name)
   erb :edit
 end
 
