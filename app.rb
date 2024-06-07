78676924a1af8d690d3bbdd8a381f7919dffbe8a
yama998877 <yamada.sss.369@gmail.com>
2024-06-07 09:12:01 +0900

    read_memoメソッドの削除
    無いほうがわかりやすいので削除。
    read_memoを使用していた所にはread_memosを代わりに使う。

--- app.rb
+++ app.rb
@@ -26,10 +26,6 @@ def read_memos
   memos
 end
 
-def read_memo(memo_uuid)
-  read_memos[memo_uuid]
-end
-
 def write_memo(uuid, memo_title, memo_detail)
   memo = {
     title: memo_title,
@@ -56,25 +52,25 @@ end
 
 get '/memos/:id' do
   @json_file = params[:id]
-  @memo_detail = read_memo(@json_file)
+  @memo_detail = read_memos[@json_file]
   erb :detail
 end
 
 patch '/memos/:id' do
   uuid = params[:id]
-  write_memo(uuid, params[:title], params[:detail]) unless read_memo(uuid).nil?
+  write_memo(uuid, params[:title], params[:detail]) unless read_memos[uuid].nil?
   redirect '/memos'
 end
 
 delete '/memos/:id' do
   uuid = params[:id]
-  File.delete("./data/#{uuid}.json") unless read_memo(uuid).nil?
+  File.delete("./data/#{uuid}.json") unless read_memos[uuid].nil?
   redirect '/memos'
 end
 
 get '/memos/:id/edit' do
   @file_name = params[:id]
-  @memo = read_memo(@file_name)
+  @memo = read_memos[@file_name]
   erb :edit
 end
 
