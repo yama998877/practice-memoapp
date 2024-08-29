9d1913170dce566d105ad9416cfe7f44d6770048
yama998877 <yamada.sss.369@gmail.com>
2024-08-29 12:50:14 +0900

    修正
    定数CONNでのデータベース接続を変数connに変更。
    read_memosメソッドをread_memos,read_memo(memo_id)に切り分け。
    read_memo(memo_id)メソッド内のクエリにLIMIT 1を追加。
    修正したメソッドに対応できるようにコードを修正。

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ require 'pathname'
 require 'pg'
 set :enviroment, :production
 
-CONN = PG.connect(dbname: 'memoapp')
+conn = PG.connect(dbname: 'memoapp')
 
 helpers do
   def h(text)
@@ -16,33 +16,33 @@ helpers do
   end
 end
 
-def read_memos(memo_id = 'none')
-  if memo_id == 'none'
-    memos = {}
-    CONN.exec('SELECT id,title,detail FROM memos') do |result|
-      result.each do |row|
-        memo = { title: row['title'], detail: row['detail'] }
-        uuid = row['id']
-        memos[uuid] = memo
-      end
-    end
-  else
-    memos = 0
-    CONN.exec_params('SELECT title,detail FROM memos WHERE id = $1', [memo_id]) do |result|
-      result.each do |row|
-        memo = { title: row['title'], detail: row['detail'] }
-        memos = memo
-      end
+def read_memos
+  memos = {}
+  conn.exec('SELECT id,title,detail FROM memos') do |result|
+    result.each do |row|
+      memo = { title: row['title'], detail: row['detail'] }
+      uuid = row['id']
+      memos[uuid] = memo
     end
   end
   memos
 end
 
+def read_memo(memo_id)
+  memo = 0
+  conn.exec_params('SELECT title,detail FROM memos WHERE id = $1 LIMIT 1', [memo_id]) do |result|
+    result.each do |row|
+      memo = { title: row['title'], detail: row['detail'] }
+    end
+  end
+  memo
+end
+
 def write_memo(uuid, memo_title, memo_detail)
   if read_memos.key?(uuid) == false
-    CONN.exec_params('INSERT INTO memos VALUES ($1,$2,$3,now())', [uuid, memo_title, memo_detail])
+    conn.exec_params('INSERT INTO memos VALUES ($1,$2,$3,now())', [uuid, memo_title, memo_detail])
   else
-    CONN.exec('UPDATE memos SET (title, detail, update_at) = ($1,$2, now()) WHERE id = $3', [memo_title, memo_detail, uuid])
+    conn.exec('UPDATE memos SET (title, detail, update_at) = ($1,$2, now()) WHERE id = $3', [memo_title, memo_detail, uuid])
   end
 end
 
@@ -61,7 +61,7 @@ end
 
 get '/memos/:id' do
   @uuid = params[:id]
-  @memo_detail = read_memos(@uuid)
+  @memo_detail = read_memo(@uuid)
   erb :detail
 end
 
@@ -73,13 +73,13 @@ end
 
 delete '/memos/:id' do
   uuid = params[:id]
-  CONN.exec('DELETE FROM memos WHERE id = $1', [uuid])
+  conn.exec('DELETE FROM memos WHERE id = $1', [uuid])
   redirect '/memos'
 end
 
 get '/memos/:id/edit' do
   @uuid = params[:id]
-  @memo = read_memos(@uuid)
+  @memo = read_memo(@uuid)
   erb :edit
 end
 
